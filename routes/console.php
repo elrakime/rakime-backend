2383407d82407747e3dee0389d435716f1db20cf
AbdelaliKDM <khademallah.abdelali@gmail.com>
2026-09-24 10:04:04 +0100

    feat(console): schedule queue worker to run every minute in the background

--- routes/console.php
+++ routes/console.php
@@ -4,8 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('queue:work --stop-when-empty --tries=3 --max-time=50')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->runInBackground();
 
 Schedule::command('contracts:sync-status')->daily();
